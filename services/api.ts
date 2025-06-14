d3d01612c66d8e51e8c2efd0b681a2aa5cb0b7ff
Hosam-Abdullah <hosam.bzns@gmail.com>
2025-06-14 19:18:28 +0300

    hosam

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 // services/api.ts
 import axios from "axios";
 
-export const baseUrl = "https://10fb-102-191-183-130.ngrok-free.app/";
+export const baseUrl = "https://053c-105-196-167-243.ngrok-free.app/";
 export const FoodCategories = {
   Seafood: 1,
   "Grilled Food & Steaks": 2,
